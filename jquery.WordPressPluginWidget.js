9bf0b2b1f75e53309aa21f644cd59e398669ec1e
Pär Thernström <par@earthpeople.se>
2013-08-24 15:54:08 +0200

    more plugins examples + fix so headline does not take over position by rating

--- jquery.WordPressPluginWidget.js
+++ jquery.WordPressPluginWidget.js
@@ -72,7 +72,7 @@ jQuery(document).ready(function($){
 			+'.wordpress-box{font-family:helvetica,arial,sans-serif;font-size:13px;line-height:18px;background:#fafafa;border:1px solid #ddd;color:#666;border-radius:3px}'
 			+'.wordpress-box a{color:#4183c4;border:0;text-decoration:none}'
 			+'.wordpress-box .wordpress-box-title{position:relative;border-bottom:1px solid #ddd;border-radius:3px 3px 0 0;background:#fcfcfc;background:-moz-linear-gradient(#fcfcfc,#ebebeb);background:-webkit-linear-gradient(#fcfcfc,#ebebeb);}'
-			+'.wordpress-box .wordpress-box-title h3{font-family:helvetica,arial,sans-serif;font-weight:normal;font-size:16px;color:gray;margin:0;padding:10px 10px 10px 30px;background:url('+box_title_png+') 7px center no-repeat}'
+			+'.wordpress-box .wordpress-box-title h3{font-family:helvetica,arial,sans-serif;font-weight:normal;font-size:16px;color:gray;margin:0 115px 0 0;padding:10px 10px 10px 30px;background:url('+box_title_png+') 7px center no-repeat;}'
 			+'.wordpress-box .wordpress-box-title h3 .repo{font-weight:bold}'
 			+'.wordpress-box .wordpress-box-title .wordpress-stats{position:absolute;top:4px;right:10px;xbackground:white;xborder:1px solid #ddd;border-radius:3px;font-size:11px;xfont-weight:bold;line-height:18px;height:18px;padding:0 .5em}'
 			+'.wordpress-box .wordpress-box-title .wordpress-stats a{display:inline-block;height:21px;color:#666;padding:0 5px 0 18px;background: url('+') no-repeat}'
